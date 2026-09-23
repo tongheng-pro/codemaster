8e718d8a765931aca4456ea76743584b2464e0f5
tongheng <hengprogram@gmail.com>
2026-09-23 16:33:08 +0700

    fixed bug

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -118,7 +118,7 @@ export default function Home({ stats }: Props) {
                                     type="submit"
                                     className="px-5 py-2.5 rounded-xl bg-primary-600 hover:bg-primary-500 text-white text-sm font-semibold transition-all shadow-sm"
                                 >
-                                    {t('nav.search_btn')}
+                                    {t('nav.search_btn')} 
                                 </button>
                             </form>
 
